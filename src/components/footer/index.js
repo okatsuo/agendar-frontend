a8bcdbd3efd18cbc973eae5c4085b8ea5b9d6a4a
igorGomes <gomesoliveirasl02@hotmail.com>
2022-04-14 14:08:20 -0300

    ui material added to project

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,3 +3,4 @@ export const Footer = () => {
     <div>Desenvolvido com ❤️ por Agendar</div>
   )
 }
+
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,40 @@
 import Head from 'next/head'
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../theme';
+import createEmotionCache from '../creat_emoticon_cache';
 
-function MyApp({ Component, pageProps }) {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps, } = props;
+
   return (
-    <>
+    <CacheProvider value={emotionCache}>
       <Head>
-        <title>Agendar</title>
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <Component {...pageProps} />
-    </>
-  )
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
+
+
+
+
+
+
